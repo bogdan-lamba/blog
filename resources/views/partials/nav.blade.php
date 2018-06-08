26970a17785584dd0b56a0f2525e45ba69f54904
Bogdan L <bogdan.lamba89@gmail.com>
2018-06-08 15:43:45 +0300

    nav back to create

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -2,7 +2,7 @@
       <div class="container">
         <nav class="nav blog-nav">
           <a class="nav-link active" href="/">All posts</a>
-          <a class="nav-link" href="/posts/create">New2 post</a>
+          <a class="nav-link" href="/posts/create">Create post</a>
         </nav>
       </div>
 </div>
