2ab71aedd8d62f799d46fd88fba3c0cd32df18c3
Bogdan L <bogdan.lamba89@gmail.com>
2018-06-08 15:50:28 +0300

    nav test

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -2,7 +2,7 @@
       <div class="container">
         <nav class="nav blog-nav">
           <a class="nav-link active" href="/">All posts</a>
-          <a class="nav-link" href="/posts/create">Create post</a>
+          <a class="nav-link" href="/posts/create">Create_test post</a>
         </nav>
       </div>
 </div>
